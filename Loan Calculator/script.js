bb834c82fbe597789a78b971ca2ae75a7949199e
patrick-hub <patrick4akuagwu@yahoo.com>
2022-07-04 22:58:29 +0100

    Loan Calculator

---
+++ Loan Calculator/script.js
@@ -0,0 +1,79 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault()
+});
+
+// calculate results
+function calculateResults() {
+// UI Variables
+const amount = document.getElementById('amount');
+const interest = document.getElementById('interest');
+const years = document.getElementById('years')
+const monthlyPayment = document.getElementById('monthly-payment');
+const totalPayment = document.getElementById('total-payment');
+const totalInterest = document.getElementById('total-interest');
+
+const principal = parseFloat(amount.value); 
+const calculatedInterest = parseFloat(interest.value) /100 / 12; // calculated interest
+const calculatedPayments = parseFloat(years.value) * 12;
+
+// Monthly Payments Computation
+const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+const monthly = (principal * x * calculatedInterest) / (x-1);
+
+// checking to see if the monthly value is a finite number using isFinite method
+if(isFinite(monthly)) {
+monthlyPayment.value = monthly.toFixed(2);
+totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+// Show results
+document.getElementById('results').style.display = 'block';
+
+// Hide Loader
+document.getElementById('loading').style.display = 'none';
+
+
+} else {
+    showError(`Please check your numbers`)
+}
+
+
+}
+
+// show Error
+function showError(error) {
+
+    // Hide results
+document.getElementById('results').style.display = 'none';
+
+// Hide Loader
+document.getElementById('loading').style.display = 'none';
+    // create a div
+    const errorDiv = document.createElement('div');
+
+    // Get Elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // insert Error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // clear Error After 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 2000);
+}
+
